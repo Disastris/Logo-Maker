d6a0aeae3a3ee3dbb357237d6ecd9112fe98080a
Disastris <chavez.garcia.santi@gmail.com>
2023-10-17 01:10:35 -0500

    Wrote a prompt for inquirer on index.js

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+inquirer
+    .prompt([
+    {   type: 'input',
+        message: 'You can only fit three characters on your logo. Please choose wisely:',
+        name: 'text',
+    },
+    {   type:'list',
+        message: 'What color do you want the shape to be',
+        choices: []
+    },
+    {
+        type: 'list',
+        message: 'What shape do you want the logo to be',
+        choices:['Square','Triangle','Circle']
+    },
+    ])
+    .then((answers) => {
+        const logoContent = generatedLogo(answers);
+
+        fs.writeFile('New Logo', logoContent, (err)=>
+        err ? console.log(err) : console.log('Succesfully created your new Logo!'))
+    });
